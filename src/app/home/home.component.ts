79afb73f404e65ffefaeaef8d5df9ebdac1629eb
zach taylor <taylor67469@yahoo.com>
2022-03-12 14:46:01 -0600

    cards working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,16 +8,28 @@ import axios from "axios"
 })
 export class HomeComponent implements OnInit {
   constructor() { }
-
+  name:any;
+  images:any;
   ngOnInit(): void {
   }
   locationChange(data: any){
     axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?city=${data}&apikey=wjwMPXOUtVFerXr1XhACcVNydgAZXEbH`)
-  .then(function (response) {
+  .then( (response) => {
     // handle success
-    console.log(response);
+    // console.log(response.data._embedded.events.map((e: any)=>{
+    //   console.log(e.name);
+
+    // }))
+    this.name=response.data._embedded.events.map((e: { name: any; })=>{
+      return e.name;  
+    })
+    // this.images=response.data._embedded.events.map((e: { images: any; })=>{
+    //   e.images[0].url;
+    // })
+    // this.images=response.data._embedded.events.map((e: { images: any; })=>{
+    //   return e.images.url;
+    // })
   })
-    
   }
   dateChange(data: any){
     console.log(data);
