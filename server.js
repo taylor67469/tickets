2ee6a74f52ca7ef1ece343c11eec84752143c725
zach taylor <taylor67469@yahoo.com>
2022-03-13 22:46:56 -0500

    yay finished with heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<tickets>'));
+app.use(express.static(__dirname + '/dist/tickets'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<tickets>/index.html'));});
+'/dist/tickets/index.html'));});
 app.listen(process.env.PORT || 8080);
