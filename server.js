7f682ee4c327df6c04056f68e5d6b1b58bb54932
zach taylor <taylor67469@yahoo.com>
2022-03-13 22:37:14 -0500

    working?

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<tickets>'));
+app.use(express.static(__dirname + '/dist/<ticketspercity>'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+'/dist/<ticketspercity>/index.html'));});
 app.listen(process.env.PORT || 8080);
